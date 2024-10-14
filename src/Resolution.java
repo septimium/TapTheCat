54ae3712f1406d60d17965cb6347faea739d4357
septimium <septimiutg@gmail.com>
2024-10-14 21:14:44 +0300

    Modified Resolution Selector

--- src/Resolution.java
+++ src/Resolution.java
@@ -1,45 +1,55 @@
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Resolution {
     public Resolution() {
+        //Custom Font
+        Font font = null;
+        try {
+            InputStream is = getClass().getResourceAsStream("/FFFForward.TTF");
+            font = Font.createFont(Font.TRUETYPE_FONT, is);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, is));
+        } catch (IOException | FontFormatException e) {
+        }
+
         JFrame res = new JFrame();
-        res.setSize(300, 500);
-
-        JPanel buttons = new JPanel();
-        buttons.setSize(300, 200);
-        buttons.setLayout(new GridBagLayout());
-
-        GridBagConstraints c = new GridBagConstraints();
+        res.setTitle("TapTheCat: Choose Resolution");
+        res.setSize(400, 600);
+        res.setLayout(new GridLayout(3,1));
+        JLabel text = new JLabel("<HTML><CENTER>Choose resolution:</CENTER><HTML>");
+        text.setFont(font.deriveFont(Font.PLAIN, 40));
+        res.add(text);
 
         JButton b1 = new JButton("1280x720 | HD");
-        b1.setPreferredSize(new Dimension(200, 50));
-        c.ipadx = 86;
-        c.ipady = 20;
-        c.anchor = GridBagConstraints.CENTER;
-        c.gridwidth = GridBagConstraints.REMAINDER;
+        b1.setFont(font.deriveFont(Font.PLAIN, 18));
+        b1.setFocusPainted(false);
+        b1.setSize(200,50);
         b1.addActionListener(e -> {
-//            res.dispose();
+            res.dispose();
             UI u = new UI(1.5,1280,720);
         });
-        buttons.add(b1, c);
+        res.add(b1);
 
 
-        JButton b2 = new JButton("1920x1080 | HD");
-        b2.setPreferredSize(new Dimension(200, 50));
-        c.ipadx = 86;
-        c.ipady = 20;
-        c.anchor = GridBagConstraints.CENTER;
-        c.gridwidth = GridBagConstraints.REMAINDER;
+        JButton b2 = new JButton("1920x1080 | FULL HD");
+        b2.setFont(font.deriveFont(Font.PLAIN, 18));
+        b2.setFocusPainted(false);
+        b2.setSize(200,50);
         b2.addActionListener(e -> {
-//            res.dispose();
+            res.dispose();
             UI u = new UI(2,1920,1080);
         });
-        buttons.add(b2, c);
+        res.add(b2);
 
-        res.add(buttons, BorderLayout.SOUTH);
+
         res.setResizable(false);
         res.setLocationRelativeTo(null);
         res.setVisible(true);
