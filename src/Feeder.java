0705561b2b16897ed09a2e1084791b40a4ff5ca7
septimium <septimiutg@gmail.com>
2024-10-18 14:03:28 +0300

    Version alpha1.6

---
+++ src/Feeder.java
@@ -0,0 +1,2 @@
+public class Feeder {
+}
--- src/TapTheCat.java
+++ src/TapTheCat.java
@@ -1,6 +1,6 @@
 public class TapTheCat {
     public static void main(String[] args){
-        System.out.println("Version: alpha1.5");
+        System.out.println("Version: alpha1.6");
         new Menu();
     }
 }
